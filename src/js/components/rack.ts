eaf06d9fcae008535d159d5295ed7f042137388a
Pimskie <pimskiedev@gmail.com>
2024-07-04 16:57:56 +0200

    feat: styling the rack and pack

--- src/js/components/rack.ts
+++ src/js/components/rack.ts
@@ -1,5 +1,5 @@
 import { customElement } from 'lit/decorators.js';
-import { html, LitElement } from 'lit';
+import { css, html, LitElement } from 'lit';
 import '@/components/row';
 
 import { Sample } from '@/types/Sample';
@@ -8,10 +8,20 @@ const samples: Sample[] = Object.values(Sample);
 
 @customElement('x-rack')
 export default class Rack extends LitElement {
+  static styles = css`
+    .rack {
+      display: grid;
+      gap: var(--size-6);
+    }
+  `;
+
   render() {
     return html`
       <h1>Samples</h1>
-      ${samples.map((sample) => html`<x-row sample="${sample}" />`)}
+
+      <div class="rack">
+        ${samples.map((sample) => html`<x-row sample="${sample}" />`)}
+      </div>
     `;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
 import './css/style.css';
-import logo from './logo.svg';
 
 import '@/app';
