77cd4915309335a8838db8c4cd242731079c9cb8
Pimskie <pimskiedev@gmail.com>
2024-07-05 15:11:38 +0200

    feat: tempo slider

--- src/js/components/sequencer.ts
+++ src/js/components/sequencer.ts
@@ -1,7 +1,9 @@
 import { customElement, property, state } from 'lit/decorators.js';
 import { html, css, LitElement } from 'lit';
+import { Ref, ref, createRef } from 'lit/directives/ref.js';
 
 import { getNextStep } from '@/utils/stepper';
+import { getTempo } from '@/utils/get-tempo';
 
 @customElement('x-sequencer')
 export default class Sequencer extends LitElement {
@@ -13,22 +15,48 @@ export default class Sequencer extends LitElement {
 
       text-align: center;
     }
+
+    .sequencer__step {
+      width: 100%;
+      height: 2rem;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      border-radius: var(--radius-2);
+      background: transparent;
+
+      transition: background-color 0.15s var(--ease-in-3);
+    }
+
+    .sequencer__step.is-active {
+      background-color: var(--red-12);
+    }
+
+    .sequencer__controls {
+      display: flex;
+      gap: var(--size-3);
+    }
   `;
 
   @property({ type: Number })
   steps: number = 10;
 
-  @property({ type: Number })
-  tempo: number = 500;
-
   @state()
   private _isActive: boolean = false;
+
+  @state()
+  tempo: number = 500;
 
   @state()
   private _currentStep: number | undefined = undefined;
 
   @state()
   _intervalId: number | undefined = undefined;
+
+  @state()
+  protected _tempoElement: Ref<HTMLInputElement> = createRef();
 
   private _toggle() {
     this._isActive = !this._isActive;
@@ -37,6 +65,8 @@ export default class Sequencer extends LitElement {
   }
 
   private _start() {
+    clearInterval(this._intervalId);
+
     this._intervalId = setInterval(() => {
       this._updateStep();
     }, this.tempo);
@@ -60,17 +90,46 @@ export default class Sequencer extends LitElement {
     this.dispatchEvent(event);
   }
 
+  private _onTempoSliderChanged() {
+    const sliderValue = this._tempoElement.value!.valueAsNumber;
+
+    this.tempo = getTempo(sliderValue);
+
+    this._start();
+  }
+
   protected render() {
     const stepsElements = new Array(this.steps)
       .fill(0)
-      .map((_, i) => html` <div class="squencer__step">${i + 1}</div> `);
+      .map(
+        (_, i) => html`
+          <div
+            class="sequencer__step ${this._currentStep === i
+              ? css`is-active`
+              : css``}"
+          >
+            ${i + 1}
+          </div>
+        `,
+      );
 
     return html`
       <div class="sequencer" style="--steps: ${this.steps}">
         <div class="sequencer__steps">${stepsElements}</div>
         <div class="sequencer__controls">
-          <button @click="${this._toggle}">${this._isActive}</button>
-          (${this._currentStep})
+          <div>
+            <button @click="${this._toggle}">${this._isActive}</button>
+            (${this._currentStep})
+          </div>
+
+          <input
+            ${ref(this._tempoElement)}
+            type="range"
+            min="0"
+            max="1"
+            step="0.1"
+            @input="${this._onTempoSliderChanged}"
+          />
         </div>
       </div>
     `;
